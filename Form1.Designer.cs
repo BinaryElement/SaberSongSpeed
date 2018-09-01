1e93f1e1d188ed1f8df8ea6081fc54d343159deb
BinaryElement <33382750+BinaryElement@users.noreply.github.com>
2018-09-01 06:45:50 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SaberSongSpeed {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mapTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.multiplierInput = new System.Windows.Forms.TextBox();
+            this.multiplierLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mapTextBox
+            // 
+            this.mapTextBox.Location = new System.Drawing.Point(12, 59);
+            this.mapTextBox.Multiline = true;
+            this.mapTextBox.Name = "mapTextBox";
+            this.mapTextBox.Size = new System.Drawing.Size(776, 379);
+            this.mapTextBox.TabIndex = 0;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(628, 12);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(160, 41);
+            this.convertButton.TabIndex = 1;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // multiplierInput
+            // 
+            this.multiplierInput.Location = new System.Drawing.Point(12, 33);
+            this.multiplierInput.Name = "multiplierInput";
+            this.multiplierInput.Size = new System.Drawing.Size(160, 20);
+            this.multiplierInput.TabIndex = 2;
+            // 
+            // multiplierLabel
+            // 
+            this.multiplierLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.multiplierLabel.Location = new System.Drawing.Point(13, 12);
+            this.multiplierLabel.Name = "multiplierLabel";
+            this.multiplierLabel.Size = new System.Drawing.Size(160, 18);
+            this.multiplierLabel.TabIndex = 3;
+            this.multiplierLabel.Text = "Multiplier";
+            this.multiplierLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Location = new System.Drawing.Point(180, 13);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(442, 40);
+            this.infoLabel.TabIndex = 4;
+            this.infoLabel.Text = resources.GetString("infoLabel.Text");
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.multiplierLabel);
+            this.Controls.Add(this.multiplierInput);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.mapTextBox);
+            this.Name = "Form1";
+            this.Text = "Saber Song Speed";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mapTextBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.TextBox multiplierInput;
+        private System.Windows.Forms.Label multiplierLabel;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaberSongSpeed {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+            mapTextBox.MaxLength = int.MaxValue;
+        }
+
+        private void convertButton_Click(object sender, EventArgs e) {
+
+            string[] multiplierLines = multiplierInput.Lines;
+
+            float multiplier;
+            try {
+                if (multiplierLines.Length > 0) {
+                    if (!float.TryParse(multiplierLines[0], out multiplier)) {
+                        infoLabel.Text = "Your multiplier value is invalid!  You must use a valid decimal number (above zero).";
+                        return;
+                    }
+                } else {
+                    infoLabel.Text = "Your multiplier value is invalid!  You must use a valid decimal number (above zero).";
+                    return;
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+                infoLabel.Text = "Your multiplier value is invalid!  You must use a valid decimal number (above zero).";
+                return;
+            }
+
+            string[] maps = mapTextBox.Lines;
+
+            if (maps.Length < 1) {
+                infoLabel.Text = "You must include the map data you wish to convert!";
+                return;
+            }
+
+            try {
+
+                string[] newMaps = new string[maps.Length];
+                for (int i = 0; i < maps.Length; i++) {
+                    string[] subMap = maps[i].Split('{');
+                    for (int j = 0; j < subMap.Length; j++) {
+                        string s = subMap[j];
+                        if (s.StartsWith("\"_time\":")) {
+                            s = s.Split(',')[0].Split(':')[1];
+                            float t = float.Parse(s);
+                            subMap[j] = subMap[j].Replace("time\":" + t, "time\":" + (t / multiplier));
+
+                            if (subMap[j].Contains("_duration")) {
+                                s = subMap[j].Split(':')[4].Split(',')[0];
+                                t = float.Parse(s);
+                                subMap[j] = subMap[j].Replace("duration\":" + t, "duration\":" + (t / multiplier));
+                            }
+                            //Console.WriteLine(s);
+                        }
+                    }
+                    newMaps[i] = string.Join("{", subMap);
+                }
+
+                mapTextBox.Lines = newMaps;
+
+                infoLabel.Text = "Done!";
+
+            } catch (Exception ex) {
+                infoLabel.Text = "There was an error during conversion!";
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
